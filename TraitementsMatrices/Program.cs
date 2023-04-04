503c5e04f807dae234730a69be5d4979beac92de
Mahy Clément <210305@CCM-Asty-Moulin.local>
2023-04-04 12:06:49 +0200

    1

---
+++ TraitementsMatrices/Program.cs
@@ -0,0 +1,87 @@
+﻿using System;
+
+namespace TraitementsMatrices
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            int L1;
+            do
+            {
+                do Console.WriteLine("Nombre de lignes(entre 1 à 10) de la première matrice. ");
+                while (!int.TryParse(Console.ReadLine(), out L1));
+            } while (L1 < 1 || L1 > 10);
+            int C1;
+            do
+            {
+                do Console.WriteLine("Nombre de colonnes(entre 1 à 10) de la première matrice.");
+                while (!int.TryParse(Console.ReadLine(), out C1));
+            } while (C1 < 1 || C1 > 10);
+            int L2;
+            do
+            {
+                do Console.WriteLine("Nombre de lignes(entre 1 à 10) de la deuxième matrice.");
+                while (!int.TryParse(Console.ReadLine(), out L2));
+            } while (L2 < 1 || L2 > 10);
+            int C2;
+            do
+            {
+                do Console.WriteLine("Nombre de colonnes(entre 1 à 10) de la deuxième matrice.");
+                while (!int.TryParse(Console.ReadLine(), out C2));
+            } while (C2 < 1 || C2 > 10);
+            Console.WriteLine("\n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n");
+            Console.WriteLine("-----------------------------------------");
+            Console.WriteLine("Nombres de lignes de la première matrice -> "+L1);
+            Console.WriteLine("Nombres de colonnes de la première matrice -> " + C1);
+            Console.WriteLine("Nombres de lignes de la deuxième matrice -> " + L2);
+            Console.WriteLine("Nombres de colonnes de la deuxième matrice -> " + C2);
+            Console.WriteLine("-----------------------------------------");
+            Console.WriteLine("");
+
+            int[,] M2; //Deuxième matrice
+            string stringTab1; //Concaténation de la première matrice
+            string stringTab2; // Concaténation de la deuxième matrice
+            int[,] M1; // Première matrice
+            int[,] MR; // Résultat de l'addition
+            bool R; // Booléen de vérification de l'addition
+            bool verifBool; // Booléen de vérification de la multiplication
+            int[,] result; // Résultat de la multiplication
+            string stringMR; // Concaténation de l'addition
+            string stringResult; // Concaténation de la multiplication
+            sousProgrammes sousProgrammes = new sousProgrammes();
+
+
+            sousProgrammes.RemplissageMatrice(L1, C1, L2, C2, out M1, out M2);
+            sousProgrammes.ConcatenationMatrice(M1, M2, L1, C1, L2, C2, out stringTab1, out stringTab2);
+            Console.WriteLine("Première matrice :");
+            Console.WriteLine(stringTab1);
+            Console.WriteLine("");
+            Console.WriteLine("Deuxième matrice :");
+            Console.WriteLine(stringTab2);
+            sousProgrammes.AdditionMatrices(L1, C1, L2, C2, M1, M2,out MR, out R );
+            sousProgrammes.MultiplicationMatrices(M1, M2, out verifBool, out result);
+            sousProgrammes.ConcatenationResultat(L1, C1, L2, C2, MR, result, out stringMR, out stringResult);
+            Console.WriteLine("Addition :");
+            if (R == true)
+            {
+                Console.WriteLine(stringMR);
+            }
+            else
+            {
+                Console.WriteLine("Addition impossible.");
+            }
+            Console.WriteLine("");
+            Console.WriteLine("Multiplication :");
+            if (verifBool == true)
+            {
+                Console.WriteLine(stringResult);
+            }
+            else
+            {
+                Console.WriteLine("Multiplication impossible.");
+            }
+        }
+    }
+}
---
+++ TraitementsMatrices/sousProgrammes.cs
@@ -0,0 +1,121 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TraitementsMatrices
+{
+    public struct sousProgrammes
+    {
+        public void RemplissageMatrice(int L1, int C1, int L2, int C2, out int[,] M1, out int[,] M2)
+        {
+            Random n = new Random();
+            M1 = new int[L1, C1];
+            M2 = new int[L2, C2];
+            for (int i = 0; i < L1; i++)
+            {
+                for(int j = 0; j < C1; j++)
+                {
+                    M1[i, j] = n.Next(0, 21);
+                }
+            }
+
+            for (int i = 0; i < L2; i++)
+            {
+                for (int j = 0; j < C2; j++)
+                {
+                    M2[i, j] = n.Next(0, 21);
+                }
+            }
+        }
+
+        public void ConcatenationMatrice(int[,] M1, int[,] M2, int L1, int C1, int L2, int C2, out string stringTab1, out string stringTab2)
+        {
+            stringTab1 = "";
+            stringTab2 = "";
+
+            for (int i = 0; i < L1; i++)
+            {
+                for(int j = 0; j < C1 ; j++)
+                {
+                    stringTab1 += M1[i, j] + "|";
+                }
+                stringTab1 += "\n";
+            }
+
+            for (int i = 0; i < L2; i++)
+            {
+                for (int j = 0; j < C2; j++)
+                {
+                    stringTab2 += M2[i, j] + "|";
+                }
+                stringTab2 += "\n";
+            }
+        }
+
+        public void AdditionMatrices(int L1, int C1, int L2, int C2, int[,] M1, int[,] M2,out int[,] MR, out bool R)
+        {
+            R = true;
+            MR = new int[L1, C2];
+            if (M1.GetLength(0) == M2.GetLength(0) && M1.GetLength(1) == M2.GetLength(1))
+            {
+                for(int i = 0; i < L1; i++)
+                {
+                    for (int j = 0; j < C2; j++)
+                    {
+                        MR[i,j] = M1[i,j] + M2[i,j];
+                    }
+                }
+            }
+            else
+            {
+                R = false;
+            }
+        }
+
+        public void MultiplicationMatrices(int[,] M1, int[,] M2,out bool verifBool, out int[,] result)
+        {
+            verifBool  = true;
+            result = new int[M1.GetLength(0), M2.GetLength(1)];
+            if(M1.GetLength(1) == M2.GetLength(0))
+            {
+                for(int i = 0; i < M1.GetLength(0); i++)
+                {
+                    for(int j = 0; j < M2.GetLength(1); j++)
+                    {
+                        result[i, j] = 0;
+                        for(int k = 0; k < M1.GetLength(1); k++)
+                        {
+                            result[i, j] = result[i, j] + M1[i, k] * M2[k, j];
+                        }
+                    }
+                }
+            }
+            else
+            {
+                verifBool = false;
+            }
+        }
+
+        public void ConcatenationResultat(int L1, int C1, int L2, int C2, int[,] MR, int[,] result, out string stringMR, out string stringResult)
+        {
+            stringMR = "";
+            stringResult = "";
+            for (int i = 0; i < MR.GetLength(0); i++)
+            {
+                for (int j = 0; j < MR.GetLength(1); j++)
+                {
+                    stringMR += MR[i, j] + "|";
+                }
+                stringMR += "\n";
+            }
+            for (int i = 0; i < result.GetLength(0); i++)
+            {
+                for (int j = 0; j < MR.GetLength(1); j++)
+                {
+                    stringResult += result[i, j] + "|";
+                }
+                stringResult += "\n";
+            }
+        }
+    }
+}
